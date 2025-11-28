0a8e143fd6a1875a978f5e1fd5d4450f153ba649
Gabriel Favero <gabriel290974@gmail.com>
2025-11-28 18:58:53 -0300

    comentado pois não foi implementado ainda

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -6,6 +6,6 @@ const router = Router();
 router.get('/create-category', CategoryController.createCategory);
 router.post('/store-category', CategoryController.storeCategory);
 router.get('/list-categories', CategoryController.listCategories)
-router.get('/list-categories/:id', CategoryController.listCategory)
+// router.get('/list-categories/:id', CategoryController.listCategory)
 
 module.exports = router;
