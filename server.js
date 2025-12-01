4f106c646f66a128a4df5bf25e634cc212eac97a
Gabriel Favero <gabriel290974@gmail.com>
2025-11-30 22:43:42 -0300

    alterando configuração de sincronização

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const sequelize = require('./src/config/database');
 
 const PORT = process.env.PORT || 3000;
 
-sequelize.sync({ alter: true }) // cria/atualiza tabelas automaticamente
+sequelize.sync() // cria/atualiza tabelas automaticamente
     .then(() => {
         console.log('Banco sincronizado com sucesso!');
         app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
