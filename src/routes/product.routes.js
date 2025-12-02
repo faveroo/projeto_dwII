f9da0d8f0ae9f068789a41aecf73870e16968b9e
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-12-02 15:31:15 -0300

    Adicionada a rota de detalhes de produto e incluindo parametro de imagem no store

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 const ProductController = require('../controllers/ProductController')
+const upload = require('../middlewares/upload')
 
 const router = Router()
 
 router.get('/create-product', ProductController.createProduct)
-router.post('/store-product', ProductController.storeProduct)
+router.post('/store-product', upload.single('image'), ProductController.storeProduct)
 router.get('/list-products', ProductController.indexProduct)
+router.get('/product-detail/:id', ProductController.showProduct)
 
 module.exports = router
